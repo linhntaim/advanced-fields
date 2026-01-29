d88418bf4bd42168eb8f1ba74dadb46c6a2de629
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2026-01-29 18:48:05 +0700

    fix lazy loading css

--- resources/views/checkbox-grid.blade.php
+++ resources/views/checkbox-grid.blade.php
@@ -14,60 +14,61 @@
     :component="$fieldWrapperView"
     :field="$field"
     class="fi-fo-field-grid-wrp"
-    x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('grid-fields', package: 'linhntaim/advanced-fields'))]"
 >
-    <x-filament::input.wrapper
-        :disabled="$isDisabled"
-        :valid="! $errors->has($statePath) && ! $errors->has($statePath . '*')"
-        :attributes="
-        \Filament\Support\prepare_inherited_attributes($extraAttributeBag)
-            ->class([
-                'fi-fo-checkbox',
-                'fi-fo-field-grid',
-            ]),
-        "
-    >
-        <div class="fi-fo-field-grid-table-ctn">
-            <table class="fi-fo-field-grid-table">
-                <thead>
-                <tr>
-                    <th scope="col"></th>
-                    @foreach ($options as $value => $label)
-                        <th scope="col"{!! $isOptionDisabled($value, $label) ? ' class="fi-fo-field-grid-disabled"' : '' !!}>{{ $label }}</th>
-                    @endforeach
-                </tr>
-                </thead>
-                <tbody>
-                @foreach ($rows as $rowValue => $rowLabel)
+    <div x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('grid-fields', package: 'linhntaim/advanced-fields'))]">
+        <x-filament::input.wrapper
+            :disabled="$isDisabled"
+            :valid="! $errors->has($statePath) && ! $errors->has($statePath . '*')"
+            :attributes="
+            \Filament\Support\prepare_inherited_attributes($extraAttributeBag)
+                ->class([
+                    'fi-fo-checkbox',
+                    'fi-fo-field-grid',
+                ]),
+            "
+        >
+            <div class="fi-fo-field-grid-table-ctn">
+                <table class="fi-fo-field-grid-table">
+                    <thead>
                     <tr>
-                        <th scope="row">{{ $rowLabel }}</th>
+                        <th scope="col"></th>
                         @foreach ($options as $value => $label)
-                            <td>
-                                <input
-                                    type="checkbox"
-                                    {{
-                                        $extraInputAttributeBag
-                                            ->merge([
-                                                'disabled' => $isDisabled || $isOptionDisabled($value, $label),
-                                                'id' => $id . '-' . $rowValue . '-' . $value,
-                                                'name' => $id . '.' . $rowValue . '.' . $value,
-                                                'value' => $value,
-                                                'wire:loading.attr' => 'disabled',
-                                                $wireModelAttribute => $statePath . '.' . $rowValue . '.' . $value,
-                                            ], escape: false)
-                                            ->class([
-                                                'fi-checkbox-input',
-                                                'fi-valid' => ! $errors->has($statePath),
-                                                'fi-invalid' => $errors->has($statePath),
-                                            ])
-                                    }}
-                                />
-                            </td>
+                            <th scope="col"{!! $isOptionDisabled($value, $label) ? ' class="fi-fo-field-grid-disabled"' : '' !!}>{{ $label }}</th>
                         @endforeach
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-    </x-filament::input.wrapper>
+                    </thead>
+                    <tbody>
+                    @foreach ($rows as $rowValue => $rowLabel)
+                        <tr>
+                            <th scope="row">{{ $rowLabel }}</th>
+                            @foreach ($options as $value => $label)
+                                <td>
+                                    <input
+                                        type="checkbox"
+                                        {{
+                                            $extraInputAttributeBag
+                                                ->merge([
+                                                    'disabled' => $isDisabled || $isOptionDisabled($value, $label),
+                                                    'id' => $id . '-' . $rowValue . '-' . $value,
+                                                    'name' => $id . '.' . $rowValue . '.' . $value,
+                                                    'value' => $value,
+                                                    'wire:loading.attr' => 'disabled',
+                                                    $wireModelAttribute => $statePath . '.' . $rowValue . '.' . $value,
+                                                ], escape: false)
+                                                ->class([
+                                                    'fi-checkbox-input',
+                                                    'fi-valid' => ! $errors->has($statePath),
+                                                    'fi-invalid' => $errors->has($statePath),
+                                                ])
+                                        }}
+                                    />
+                                </td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </x-filament::input.wrapper>
+    </div>
 </x-dynamic-component>
--- resources/views/radio-grid.blade.php
+++ resources/views/radio-grid.blade.php
@@ -13,75 +13,76 @@
 <x-dynamic-component
     :component="$fieldWrapperView"
     :field="$field" class="fi-fo-field-grid-wrp"
-    x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('grid-fields', package: 'linhntaim/advanced-fields'))]"
 >
-    <x-filament::input.wrapper
-        :disabled="$isDisabled"
-        :valid="! $errors->has($statePath) && ! $errors->has($statePath . '*')"
-        :attributes="
-            \Filament\Support\prepare_inherited_attributes($extraAttributeBag)
-                ->class([
-                    'fi-fo-radio',
-                    'fi-fo-field-grid',
-                ]),
-        "
-    >
-        <div class="fi-fo-field-grid-table-ctn">
-            <table class="fi-fo-field-grid-table">
-                <thead>
-                <tr>
-                    <th scope="col"></th>
-                    @foreach ($options as $value => $label)
-                        <th scope="col"{!! $isOptionDisabled($value, $label) ? ' class="fi-fo-field-grid-disabled"' : '' !!}>{{ $label }}</th>
-                    @endforeach
-                </tr>
-                </thead>
-                <tbody>
-                @foreach ($rows as $rowValue => $rowLabel)
+    <div x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('grid-fields', package: 'linhntaim/advanced-fields'))]">
+        <x-filament::input.wrapper
+            :disabled="$isDisabled"
+            :valid="! $errors->has($statePath) && ! $errors->has($statePath . '*')"
+            :attributes="
+                \Filament\Support\prepare_inherited_attributes($extraAttributeBag)
+                    ->class([
+                        'fi-fo-radio',
+                        'fi-fo-field-grid',
+                    ]),
+            "
+        >
+            <div class="fi-fo-field-grid-table-ctn">
+                <table class="fi-fo-field-grid-table">
+                    <thead>
                     <tr>
-                        <th scope="row">{{ $rowLabel }}</th>
+                        <th scope="col"></th>
                         @foreach ($options as $value => $label)
-                            <td>
-                                <input
-                                    type="radio"
-                                    {{
-                                        $extraInputAttributeBag
-                                            ->merge([
-                                                'disabled' => $isDisabled || $isOptionDisabled($value, $label),
-                                                'id' => $id . '-' . $rowValue . '-' . $value,
-                                                'name' => $id . '.' . $rowValue,
-                                                'value' => $value,
-                                                'wire:loading.attr' => 'disabled',
-                                                $wireModelAttribute => $statePath . '.' . $rowValue,
-                                            ], escape: false)
-                                            ->class([
-                                                'fi-radio-input',
-                                                'fi-valid' => ! $errors->has($statePath),
-                                                'fi-invalid' => $errors->has($statePath),
-                                            ])
-                                    }}
-                                />
-                            </td>
+                            <th scope="col"{!! $isOptionDisabled($value, $label) ? ' class="fi-fo-field-grid-disabled"' : '' !!}>{{ $label }}</th>
                         @endforeach
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-    </x-filament::input.wrapper>
+                    </thead>
+                    <tbody>
+                    @foreach ($rows as $rowValue => $rowLabel)
+                        <tr>
+                            <th scope="row">{{ $rowLabel }}</th>
+                            @foreach ($options as $value => $label)
+                                <td>
+                                    <input
+                                        type="radio"
+                                        {{
+                                            $extraInputAttributeBag
+                                                ->merge([
+                                                    'disabled' => $isDisabled || $isOptionDisabled($value, $label),
+                                                    'id' => $id . '-' . $rowValue . '-' . $value,
+                                                    'name' => $id . '.' . $rowValue,
+                                                    'value' => $value,
+                                                    'wire:loading.attr' => 'disabled',
+                                                    $wireModelAttribute => $statePath . '.' . $rowValue,
+                                                ], escape: false)
+                                                ->class([
+                                                    'fi-radio-input',
+                                                    'fi-valid' => ! $errors->has($statePath),
+                                                    'fi-invalid' => $errors->has($statePath),
+                                                ])
+                                        }}
+                                    />
+                                </td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </x-filament::input.wrapper>
 
-    @if(!$isRequired())
-        <div
-            class="fi-fo-field-grid-clear-btn-ctn"
-            x-show="{{ implode('||', array_map(fn($rowValue) => "\$wire.\$get('$statePath')['$rowValue']", array_keys($rows))) }}"
-        >
-            <button
-                class="fi-color fi-color-primary fi-text-color-700 dark:fi-text-color-400 fi-link fi-size-md fi-ac-link-action fi-fo-field-grid-clear-btn"
-                type="button"
-                x-on:click="$wire.$set('{{ $statePath }}', {})"
+        @if(!$isRequired())
+            <div
+                class="fi-fo-field-grid-clear-btn-ctn"
+                x-show="{{ implode('||', array_map(fn($rowValue) => "\$wire.\$get('$statePath')['$rowValue']", array_keys($rows))) }}"
             >
-                {{ $getClearButtonLabel() }}
-            </button>
-        </div>
-    @endif
+                <button
+                    class="fi-color fi-color-primary fi-text-color-700 dark:fi-text-color-400 fi-link fi-size-md fi-ac-link-action fi-fo-field-grid-clear-btn"
+                    type="button"
+                    x-on:click="$wire.$set('{{ $statePath }}', {})"
+                >
+                    {{ $getClearButtonLabel() }}
+                </button>
+            </div>
+        @endif
+    </div>
 </x-dynamic-component>
